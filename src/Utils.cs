56b3c674a1567b0863568e9b5f4c45be8e64d3fd
MaximilianMcC <max@maximilian.co.nz>
2026-01-20 12:19:28 +1300

    can draw text in the centre of the screen

---
+++ src/Utils.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+using Raylib_cs;
+
+static class Utils
+{
+	public static void DrawCentreText(string text, float fontSize)
+	{
+		Vector2 size = Raylib.MeasureTextEx(Raylib.GetFontDefault(), text, fontSize, fontSize / 10);
+
+		Raylib.DrawTextPro(
+			Raylib.GetFontDefault(),
+			text,
+			Raylib.GetScreenCenter(),
+			size * 0.5f,
+			0f,
+			fontSize,
+			fontSize / 10,
+			Color.White
+		);
+	}
+}
