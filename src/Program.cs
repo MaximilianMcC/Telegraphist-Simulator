dfc7c2dfc094688d75434d95ab16871816d9b125
MaximilianMcC <max@maximilian.co.nz>
2026-01-19 22:30:49 +1300

    repo setup

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Raylib.InitWindow(854, 480, "Far away drawings");
+		Raylib.InitAudioDevice();
+
+		while (!Raylib.WindowShouldClose())
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Black);
+			Raylib.DrawText("morsing the code rn", 10, 10, 30, Color.White);
+			Raylib.EndDrawing();
+		}
+
+		Raylib.CloseAudioDevice();
+		Raylib.CloseWindow();
+	}
+}
